e80b1150ad6d26440f5f0e429ac8acd7fa015ec5
Radhi NASSER <radhi@collective.work>
2024-04-02 03:01:17 +0200

    chore: rollback swagger ui code for prevernting redirection as it's not working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  DocumentBuilder,
-  SwaggerCustomOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,34 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('docs', app, document);
-
-  const options: SwaggerCustomOptions = {
-    explorer: true, // Set to false if you don't want Swagger UI to be interactive
-    customSiteTitle: 'Your API Documentation',
-    // Add custom JavaScript to manipulate Swagger UI behavior
-    // customCss: fs.readFileSync('./custom.css').toString(),
-    // customJs: fs.readFileSync('./src/custom.js').toString(),
-    customJs: `
-    document.addEventListener('DOMContentLoaded', function () {
-      // Disable automatic redirection in Swagger UI
-      const handleRedirection = () => {
-        const execute = window.SwaggerUIBundle.ui.execute;
-        window.SwaggerUIBundle.ui.execute = function () {
-          if (arguments[0] === 'requestRedirect') {
-            return;
-          }
-          return execute.apply(this, arguments);
-        };
-      };
-
-      handleRedirection();
-    });
-
-  `,
-  };
-
-  SwaggerModule.setup('docs', app, document, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(4000);
 }
